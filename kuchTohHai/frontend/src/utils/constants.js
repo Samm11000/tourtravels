38dab368f35b0e7108a4cf72061b4162ed102159
Swyam Yadav <swyamyadav1407@gmail.com>
2025-07-10 12:46:34 +0530

    made changes in frontend

--- kuchTohHai/frontend/src/utils/constants.js
+++ kuchTohHai/frontend/src/utils/constants.js
@@ -1,4 +1,5 @@
-export const BASE_URL = "http://localhost:8000/api";
+// export const BASE_URL = "http://localhost:8000/api";
+const BASE_URL = import.meta.env.VITE_BASE_URL;
 
 export const CAR_API = `${BASE_URL}/cars`;
 export const ENQUIRY_API = `${BASE_URL}/enquiry`;
