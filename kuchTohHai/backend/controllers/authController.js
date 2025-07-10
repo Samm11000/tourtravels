e574084c519276474dce29a494e2d954c0e6919b
Sambhav Garg <156508582+Sambhav-gg@users.noreply.github.com>
2025-07-10 14:46:32 +0530

    Update authController.js

--- kuchTohHai/backend/controllers/authController.js
+++ kuchTohHai/backend/controllers/authController.js
@@ -20,7 +20,7 @@ export const login = async (req, res) => {
   res
     .cookie("token", token, {
       httpOnly: true,
-      secure: false,
+      secure: true,
       sameSite: "strict",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     })
