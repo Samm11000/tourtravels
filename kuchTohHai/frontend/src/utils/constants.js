bca0cab432a98ce121c47fc97c42c20f4a19fd20
Sambhav-gg <156508582+Sambhav-gg@users.noreply.github.com>
2026-06-13 16:50:16 +0530

    m

--- kuchTohHai/frontend/src/utils/constants.js
+++ kuchTohHai/frontend/src/utils/constants.js
@@ -1,5 +1,6 @@
 // export const BASE_URL = "http://localhost:8000/api";
 const BASE_URL = import.meta.env.VITE_BASE_URL;
 console.log("CURRENT API =", BASE_URL);
+console.log("NEW EC2 TEST");
 export const CAR_API = `${BASE_URL}/cars`;
 export const ENQUIRY_API = `${BASE_URL}/enquiry`;
