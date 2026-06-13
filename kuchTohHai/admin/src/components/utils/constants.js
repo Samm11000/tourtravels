f924e78e3d6216980576e657dd1dae47a3606a6d
Sambhav-gg <156508582+Sambhav-gg@users.noreply.github.com>
2026-06-13 16:27:46 +0530

    a

--- kuchTohHai/admin/src/components/utils/constants.js
+++ kuchTohHai/admin/src/components/utils/constants.js
@@ -1,6 +1,6 @@
 // export const BASE_URL = "http://localhost:8000/api";
-export const BASE_URL = "https://tourtravels-kp7z.onrender.com/api";
-// export const BASE_URL = import.meta.env.VITE_BASE_URL;
+// export const BASE_URL = "https://tourtravels-kp7z.onrender.com/api";
+export const BASE_URL = import.meta.env.VITE_BASE_URL;
 
 export const AUTH_API = `${BASE_URL}/admin`;
 export const CAR_API = `${BASE_URL}/cars`;
@@ -8,4 +8,4 @@ export const ENQUIRY_API = `${BASE_URL}/enquiry`;
 export const DRIVER_API = `${BASE_URL}/drivers`;
 
 
-export const VEHICLE_API =`${BASE_URL}/vehicles`;
+export const VEHICLE_API = `${BASE_URL}/vehicles`;
